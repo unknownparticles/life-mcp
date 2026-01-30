ad67b95d025a03ca6d029456631b9b3c49c738c8
alun <alunnb@outlook.com>
2026-01-30 21:49:38 +0800

    Add GitHub Actions workflow for auto-deployment to Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: './',
     server: {
       port: 3000,
       host: '0.0.0.0',
